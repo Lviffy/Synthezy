3879bfbadca127e38b88745accd60e315a68b5fe
Rohan Mohanta <contact.rohan.here@gmail.com>
2025-06-07 16:57:23 +0530

    first commit

---
+++ client/src/components/Credits.jsx
@@ -0,0 +1,11 @@
+import { Github } from "../assets/icons";
+
+export default function Credits() {
+  return (
+    <section className="credits">
+      <a href="https://github.com/lviffy" id="credits">
+        <Github />
+      </a>
+    </section>
+  );
+}
